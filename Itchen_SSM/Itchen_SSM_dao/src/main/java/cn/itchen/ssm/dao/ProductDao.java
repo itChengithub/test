8331c6c90e42e14d3f79abcf2d2acb3ab5e23245
chenchao <c18435607621@163.com>
2020-05-20 17:01:17 +0800

    delete project

--- Itchen_SSM/Itchen_SSM_dao/src/main/java/cn/itchen/ssm/dao/ProductDao.java
+++
@@ -1,7 +0,0 @@
-package cn.itchen.ssm.dao;
-
-import java.util.List;
-
-public interface ProductDao {
-    List<Product> findAll();
-}
--- Itchen_SSM/Itchen_SSM_service/src/main/java/cn/itchen/ssm/service/ProductService.java
+++
@@ -1,7 +0,0 @@
-package cn.itchen.ssm.service;
-
-import java.util.List;
-
-public interface ProductService {
-    List<Product> findAll();
-}
